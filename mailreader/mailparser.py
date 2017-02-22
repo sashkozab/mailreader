8cde6f11cbfa280d47e8821b7c951a7cb27cc2c6
sashkozab <pirogovich13@gmail.com>
2017-02-22 16:54:39 +0200

    init

---
+++ mailreader/mailparser.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python3
+import imaplib
+import email
+from email.parser import BytesParser
+from email import policy
+from email.header import decode_header
+import pprint
+
+def connect_to_mail(imap_server, account, passwd):
+    M = imaplib.IMAP4_SSL(imap_server)
+    M.login(account, passwd)
+    return M
+
+
+def fetch_all(connected_mail):
+    result, data = connected_mail.uid('search', None, "ALL") # search and return uids instead
+    email_uid_list = data[0].split()
+    for email_uid in email_uid_list:
+        _, email_data = connected_mail.uid('fetch', email_uid, '(RFC822)')
+        # print(connected_mail.fetch(email_uid, "(BODY[HEADER.FIELDS (FROM)])"))
+        # print(email_data[0][1])
+        print("\nEmail UID: ", email_uid)
+        yield email_data[0][1]
+
+
+class EmailComponents:
+    """
+    Class for fetching all particular components from every email message.
+    """
+    def __init__(self, mailObject):
+        self.mailObject = mailObject
+
+        self.UID = None
+        # self.from_adress = None
+        # self.from_name = None
+        # self.to_adress = []
+        # self.to_name = []
+        self.date = None
+        self.time = None
+        # self.subject = None
+        self.body_text = ""
+        self.body_html = ""
+        self.links_object = None
+        self.filenames = []
+        self.attachments = {}
+
+        self.msg = None
+
+    def init_msg_from_bytes(self):
+        """
+        Just initialize message from mailObject using message_from_bytes method 
+        of email mudule. Otherwise by default self.msg == None
+        """
+        # self.msg = email.message_from_bytes(self.mailObject)
+        self.msg = BytesParser(policy=policy.default).parsebytes(self.mailObject)
+
+    def name_adress(self, to="To"):
+        """
+        List of tuples that containe recipient's ('To') or sender's ('From') pair of (Name, Adress) each.
+        :param to: field name of mail object. Default is 'To'
+        """
+        to_value = self.msg.get_all(to, [])
+        names_adresses = self.get_decoded_adresses(to_value)
+        name, adress = [], []
+        if names_adresses:
+            for name_adress in names_adresses:
+                name.append(name_adress[0])
+                adress.append(name_adress[1])
+        return (name, adress)
+
+    def get_subject(self, subject="Subject"):
+        """
+        Return decoded email subject.
+        :param subject: field name of mail object. Default is 'Subject'
+        """
+        subject_value = self.msg.get_all(subject, [])
+        decoded_value = self.get_decoded_value(subject_value[0])
+        return decoded_value
+
+
+    def get_decoded_value(self, field_value):
+        """
+        Get and return decoded non ASCII single value from message field.
+        """
+        if field_value:
+            decoded_value = decode_header(field_value)
+            value_final = ""
+            for chunk in decoded_value:
+                value_final += chunk[0].decode(chunk[1]) if chunk[1] else chunk[0] + " "
+            return value_final
+        else:
+            return field_value
+
+    def get_decoded_adresses(self, field_value):
+        """
+        Get and return decoded non ASCII range adresses from message field value.
+        """
+        if field_value:
+            adresses = email.utils.getaddresses(field_value)
+            decoded_adresses = []
+            for adress in adresses:
+                decoded_name, decoded_adress = decode_header(adress[0]),decode_header(adress[1])
+                name_final, adress_final = "","" 
+                for dec_n in decoded_name:
+                    name_final += dec_n[0].decode(dec_n[1]) if dec_n[1] else dec_n[0] + " "
+                for dec_a in decoded_adress:
+                    adress_final += dec_a[0].decode(dec_a[1]) if dec_a[1] else dec_a[0] + " "
+                decoded_adresses.append((name_final, adress_final))
+            return decoded_adresses
+        else:
+            return field_value
+
+    def datetime(self, date="Date"):
+        """
+        Return email date. Example: 'Thu, 5 Jan 2017 21:52:41 +0200'.
+        :param date: field name of mail object. Default is 'Date'
+        """
+        date_value = self.msg.get_all(date, [])
+        return email.utils.parsedate(date_value[0])
+
+    def set_body_message(self):
+        """
+        Get body message(html or text parts or both). Doesn't return anything, but define
+        class variables self.body_html and self.body_text if exists.
+        """
+        if self.msg.is_multipart():
+            for m in self.msg.walk():
+                # print("Is attached in grab: ", m.is_attachment())
+                # [print("!!!! Attached : ", a) for a in self.msg.iter_attachments() if m.is_attachment()]
+                self.grab_text_or_html(m)
+        else:
+            self.grab_text_or_html(self.msg)
+
+    def grab_text_or_html(self, message):
+        """
+        Looking for particular message maintype and grab each  with assigning to respective class variable.
+        Also append attached filename to self.filenames list if detect.
+        """
+        if message.get_filename():
+            self.filenames.append(message.get_filename())
+            self.attachments.update({message.get_filename() : message.get_payload()})
+        elif message.get_content_type() == "text/plain":
+            self.body_text += message.get_payload(None, True).decode('utf-8') + "\n"
+        elif message.get_content_type() == "text/html":
+            self.body_html += message.get_payload(None, True).decode('utf-8')
+
---
+++ tests/test_mailparser.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python3
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from mailreader.mailparser import *
+import email_credentials as EC
+
+if __name__ == "__main__":
+    mail = connect_to_mail(EC.host, EC.adress, EC.passward)
+    pprint.pprint(mail.list())
+    # Out: list of "folders" aka labels in gmail.
+    mail.select("inbox", readonly=True) # connect to inbox.
+
+    for r in fetch_all(mail):
+        messageObj = EmailComponents(r)
+        messageObj.init_msg_from_bytes()
+        messageFull = messageObj.msg
+        decoded_to_name_adress = messageObj.name_adress()
+        decoded_from_name_adress = messageObj.name_adress("From")
+        decoded_subject = messageObj.get_subject()
+        email_date = messageObj.datetime()
+        messageObj.set_body_message()
+        html_part = messageObj.body_html
+        text_part = messageObj.body_text
+        print("\n=======================")
+        print("Content type: ", messageFull["Content-Type"])
+        print("Full message:",messageFull, "\n++++++++++++++++++++")
+        print("CLASS: ",messageFull.__class__)
+        try:
+            print("Get body: =====================>\n", messageFull.get_body(preferencelist=('related', 'html', 'plain')), "\n---------------------")
+        except AttributeError as e:
+            print(e)
+        print("maintype: ", messageObj.msg.get_content_type())
+        print("Date: ", email_date)
+        print("Names To: ", decoded_to_name_adress[0])
+        print("Adresses To: ", decoded_to_name_adress[1])
+        print("Names from: ", decoded_from_name_adress[0])
+        print("Adresses from: ", decoded_from_name_adress[1])
+        print("Subject: ", decoded_subject)
+        print("Body Text part: \n",text_part)
+        print("Body HTML part: \n",html_part)
+        print("Attached files: ", messageObj.filenames)
+        print("Get payload[0]: ", "\n====================\n",messageFull.get_payload()[0], "\n===========================")
+        print("Get payload[1]: ", "\n====================\n",messageFull.get_payload()[1], "\n===========================")
+        # [print("Attached FILENAME: ", k, "\nFile Content: \n", messageObj.attachments[k]) for k in messageObj.attachments.keys()]
+
+
+    # msg = email.message_from_bytes(r)
+    # maintype = msg.get_content_maintype()
+    # print("\n===========================")
+    # print(maintype)
+    # try:
+    #   print("Pure MESSAGE To: ",msg.get_all('To', []))
+    #   print("Pure MESSAGE From: ",msg.get_all('from', []))
+    #   print("Pure MESSAGE Subject: ",msg.get_all('subject', []))
+    #   print("Pure MESSAGE Date: ",msg.get_all('date', []))
+    #   print(decode_header(msg['From']),decode_header(msg['Subject']))
+    #   print(decode_header(msg.get_all('To', [])[0]),decode_header(msg['Date']))
+    #   print(email.utils.getaddresses(msg.get_all('To', [])))
+    #   print(email.utils.getaddresses(msg.get_all('Message-ID', [])))
+    #   print(email.utils.parsedate(msg.get_all('date', [])[0]))
+    #   print("Subject", decode_header(msg.get_all('subject', [])[0]))
+    #   print("Decode:",decode_header(email.utils.getaddresses(msg.get_all('To', []))[1][0])[0][0].decode('utf-8'))
+
+    #   print(decode_header(msg['From'])[0][0].decode('utf-8'))
+    #   print(decode_header(msg['From'])[1][0].decode('utf-8'))
+    #   print(decode_header(msg['Subject'])[0][0].decode('utf-8'))
+    #   # print("MESSAGE FROM: ",email.utils.parseaddr(decode_header(msg['From'])[0].decode('utf-8')))
+    #   # print("MESSAGE FROM: ",email.utils.parseaddr(decode_header(msg['From'])[1].decode('utf-8')))
+    # except (AttributeError,IndexError) as er:
+    #   print("EXCEPTION way: ", er,repr(decode_header(msg['From'])))
+    # print("MESSAGE FROM: ",email.utils.parseaddr(msg['From'])[0])
+    # print(msg.keys())
+    # for m in msg.walk():
+    #   print(m.get_content_type())
+    #   if m.get_content_type() == "text/plain":
+    #       payload = m.get_payload(None, True).decode('utf-8')
+    #       # print("This is text plain part:",payload)
+    #   elif m.get_content_type() == "text/html":
+    #       payload = m.get_payload(None, True).decode('utf-8')
+    #       # print("This is text HTML part:",payload)
